dd116e0e016ecbd19f2086bf1fb058a578810e1c
David-Arruda-01 <david.souza3@estudante.ifto.edu.br>
2026-03-26 11:24:49 -0300

    Update Produto.php

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,10 +6,66 @@ use Fmk\MVC\Model;
 
 class Produto extends Model
 {
+    protected $visible = [
+        'id',
+        'nome',
+        'preco',
+        'descricao',
+        'categoria_id',
+        'ativo'
+    ];
 
-
+    /**
+     * 🔗 Produto tem vários pedidos
+     */
     public function pedidos()
     {
-        return $this->belongsToMany(Pedido::class, 'pedido_produto');
+        return $this->hasMany(Pedido::class, 'produto_id');
+    }
+
+    /**
+     * 💰 Total vendido desse produto
+     */
+    public function getTotalVendidoAttribute()
+    {
+        $this->loadMissing('pedidos');
+
+        return $this->pedidos->sum(function ($pedido) {
+            return ($pedido->quantidade ?? 0) * ($pedido->valor_un ?? 0);
+        });
+    }
+
+    /**
+     * 📦 Quantidade total vendida
+     */
+    public function getQuantidadeVendidaAttribute()
+    {
+        $this->loadMissing('pedidos');
+
+        return $this->pedidos->sum('quantidade');
+    }
+
+    /**
+     * 📊 Escopo: produtos ativos
+     */
+    public function scopeAtivos($query)
+    {
+        return $query->where('ativo', true);
+    }
+
+    /**
+     * 📊 Escopo: ordenar por nome
+     */
+    public function scopeOrdenado($query)
+    {
+        return $query->orderBy('nome');
+    }
+
+    /**
+     * 🔎 Escopo: busca por nome
+     */
+    public function scopeBuscar($query, $termo)
+    {
+        return $query->where('nome', 'like', "%{$termo}%");
     }
 }
