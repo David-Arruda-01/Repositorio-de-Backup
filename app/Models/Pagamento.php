1a0d6cc536a52fc992f973048afca530afe61a22
David-Arruda-01 <david.souza3@estudante.ifto.edu.br>
2026-03-26 11:27:01 -0300

    Update Pagamento.php

--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -2,19 +2,55 @@
 
 namespace App\Models;
 
-
-use Fmk\Databases\Query;
 use Fmk\MVC\Model;
 
 class Pagamento extends Model
 {
+    protected $visible = [
+        'id',
+        'atendimento_id',
+        'valor',
+        'metodo_pagamento',
+        'data_pagamento'
+    ];
+
+    /**
+     * 🔗 Pagamento pertence a um atendimento
+     */
     public function atendimento()
     {
-        return $this->belongsTo(Atendimento::class, 'atendimentos_id');
+        return $this->belongsTo(Atendimento::class, 'atendimento_id');
     }
 
-    public function pagamentos_tipos()
+    /**
+     * 📊 Escopo: pagamentos de um atendimento
+     */
+    public function scopeDoAtendimento($query, $atendimentoId)
     {
-        return $this->hasMany(pagamento::class, 'pagamentos_tipos_id');
+        return $query->where('atendimento_id', $atendimentoId);
+    }
+
+    /**
+     * 📊 Escopo: ordenar por data
+     */
+    public function scopeOrdenado($query)
+    {
+        return $query->orderBy('data_pagamento', 'desc');
+    }
+
+    /**
+     * 💰 Formatar valor (Accessor)
+     */
+    public function getValorFormatadoAttribute()
+    {
+        return number_format($this->valor ?? 0, 2, ',', '.');
+    }
+
+    /**
+     * 💳 Método formatado
+     */
+    public function getMetodoFormatadoAttribute()
+    {
+        return ucfirst($this->metodo_pagamento);
     }
 }
