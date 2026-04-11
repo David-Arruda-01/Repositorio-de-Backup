24a182e5315d0d07e1e76cab4188d2cd49badd53
David Arruda <arrudadesouzad@gmail.com>
2026-04-11 16:56:54 -0300

    Atualização da main do template

--- framework/MVC/Controller.php
+++ framework/MVC/Controller.php
@@ -5,11 +5,17 @@ namespace Fmk\MVC;
 use Fmk\Traits\Middleware;
 use Fmk\Utils\Request;
 
-class Controller{
+class Controller
+{
     use Middleware;
 
-    public function request() {
-            return Request::getInstance(); // Use um método estático adequado
-        }
+    public function request()
+    {
+        return Request::getInstance(); // Use um método estático adequado
     }
-    
+
+    function isAdmin()
+    {
+        return ($_SESSION['user']['tipo'] ?? null) === 'admin';
+    }
+}
